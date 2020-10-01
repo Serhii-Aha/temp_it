4aa966af13625a1d3a6b17909ee71b76962a53c4
Serhii-Aha <71459115+Serhii-Aha@users.noreply.github.com>
2020-10-02 01:23:28 +0300

    Add files via upload

--- src/components/SectorsList/SectorsList.js
+++ src/components/SectorsList/SectorsList.js
@@ -6,7 +6,7 @@ function SectorsList({ title, subtitle, items }) {
     const itemsList = items.map((sector, i) => <SectorItem sector={sector} number={i + 1} key={i} />);
 
     return (
-        <section className="sectors container">
+        <section id="politics" className="sectors container">
             <h2 className="sectors-title">{title}</h2>
             <p className="sectors-subtitle">{subtitle}</p>
             <div className="sectors-list">
